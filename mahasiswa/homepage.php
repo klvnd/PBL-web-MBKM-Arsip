1b9acd94968ad668b718449d6ba5da7950cc353b
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2023-12-03 19:50:50 +0700

    update homepage

--- mahasiswa/homepage.php
+++ mahasiswa/homepage.php
@@ -5,8 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
     <link rel="stylesheet" href="../NPM/node_modules/bootstrap/dist/css/bootstrap.css">
+    <style>
+        .bg-navy {
+            background-color: #0A2640;
+        }
+    </style>
 </head>
 <body>
+<nav class="navbar navbar-light bg-navy pt-5 px-5 pb-3">
+        <div>
+            <img src="../images/MBKM Arsip1.png" width="200" height="34" alt="">
+        </div>
+        <div class="navbar-nav ml-auto flex-row">
+            <div class="container">
+                <div class="row">
+                    <div class="col mx-3">
+                        <a class="nav-link text-white" href="homepage.php">Home</a>
+                    </div>
+                    <div class="col mx-3">
+                        <a class="nav-link text-white" href="pengajuan.php">Pengajuan</a>
+                    </div>
+                    <div class="col mx-3">
+                        <a class="nav-link text-white" href="riwayat.php">Riwayat</a>
+                    </div>
+                    <div class="col mx-3">
+                        <a class="nav-link text-white" href="upload.php">Upload</a>
+                    </div>
+                </div>
+            </div>
+            <div class="rounded-pill p-2 bg-white d-inline-block" style="width: 160px;">
+                <p class="m-0 text-black text-center" style="font-weight: bold;">Username</p>
+            </div>
+        </div>
+    </nav>
+
+    <div class="bg-navy">
+        <div class="p-5 text-center text-white">
+            <h1>Magang</h1>
+            <p class="p-5 w-50 mx-auto">Fakultas Vokasi UB menerapkan magang sesuai dengan kebijakan Merdeka Belajar Kampus Merdeka (MBKM), dimana memberikan kesempatan kepada mahasiswa untuk belajar di luar kampus selama 2 semester dan dapat dikonversi 20 sks untuk setiap semester.</p>
+
+        </div>
+    </div>
     <!-- ubah kodenya dibagian bawah sini -->
     <h1>Homepage</h1>
 
